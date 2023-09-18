f726a132dd8643841c5cbfeab6f9c80b360c99bf
abcabc <abcabc@gmail.com>
2023-09-18 15:13:20 -0500

    App

---
+++ CRUDSqlite/CRUDSqlite/Models/Registro.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace CRUDSqlite.Models
+{
+    public class Registro
+    {
+        [PrimaryKey, AutoIncrement]
+        public int idUsuario { get; set; }
+        [MaxLength(100)]
+        public string usuario { get; set; }
+        [MaxLength(100)]
+        public string nombre { get; set; }
+        [MaxLength(100)]
+        public string apellidoPaterno { get; set; }
+        [MaxLength(100)]
+        public string apellidoMaterno { get; set; }
+        [MaxLength(100)]
+        public string fechNacimiento { get; set; }
+        [MaxLength(100)]
+        public string documento { get; set; } // opcional???
+        [MaxLength(100)]
+        public int celular { get; set; }
+        [MaxLength(9)]
+        public string contraseña { get; set; }
+        [MaxLength(100)]
+        public string email { get; set; }
+    }
+}
---
+++ CRUDSqlite/CRUDSqlite/Models/ServicioModel.cs
@@ -0,0 +1,29 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CRUDSqlite.Models
+{
+    public class ServicioModel
+    {
+        [PrimaryKey, AutoIncrement]
+        public int idServicios { get; set; }
+        [MaxLength(100)]
+        public string pais { get; set; }
+        [MaxLength(100)]
+        public string destino { get; set; }
+        [MaxLength(100)]
+        public DateTime fechaPartida { get; set; }
+        [MaxLength(100)]
+        public decimal precio { get; set; }
+        [MaxLength(100)]
+        public string detalles { get; set; }
+        [MaxLength(100)]
+        public DateTime horaLlegada { get; set; }
+        [MaxLength(100)]
+        public DateTime fechaHoraPartida { get; set; }
+        [MaxLength(100)]
+        public DateTime fechaHoraLlegada { get; set; }
+    }
+}
